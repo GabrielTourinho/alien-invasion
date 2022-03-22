4b78d173221a53929dad73cfc2f9d989dadf9930
gabrieltourinho <gabetourinho@gmail.com>
2022-03-22 16:52:08 -0300

    Created a button to start the game.

--- ship.py
+++ ship.py
@@ -20,14 +20,9 @@ class Ship:
         # Store a decimal value for the ship's horizontal position.
         self.x = float(self.rect.x)
 
-        # Store a decimal value for the ship's vertical position
-        self.y = float(self.rect.y)
-
         # Movement flags
         self.moving_right = False
         self.moving_left = False
-        self.moving_up = False
-        self.moving_down = False
 
     def update(self):
         """Update the ship's position based on the movement flag"""
@@ -37,15 +32,8 @@ class Ship:
         if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
 
-        # Update the ship's y value, not the rect.
-        if self.moving_up and self.rect.top > 0:
-            self.y -= self.settings.ship_vertical_speed
-        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
-            self.y += self.settings.ship_vertical_speed
-
         # Update rect object from self.x and self.y
         self.rect.x = self.x
-        self.rect.y = self.y
 
     def blitme(self):
         """Draw the ship at its current location."""
